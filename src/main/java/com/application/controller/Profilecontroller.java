c1bce65f6b4b7501e4cfe3728799a1035ed2061f
Mosharraf <talukder.csedu@gmail.com>
2021-10-03 17:48:36 +0600

    commit Spring Boot

--- src/main/java/com/application/controller/Profilecontroller.java
+++ src/main/java/com/application/controller/Profilecontroller.java
@@ -1,10 +1,13 @@
 package com.application.controller;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,14 +41,32 @@ public class Profilecontroller {
 	}
 	
 	@PutMapping("/api/Update/{targetode}")
-	public TargetMaster UpdateTarget(@PathVariable String targetode,@RequestBody TargetMaster targetmaster) {		
-		TargetMaster master=targetservice.findById(targetode).orElseThrow();
-		master.setStart_date(targetmaster.getStart_date());
-		master.setActive_flag(targetmaster.getActive_flag());
-		master.setEnd_date(targetmaster.getEnd_date());
-		master.setTarget_description(targetmaster.getTarget_description());
-		master.setParent_code(targetmaster.getParent_code());
-		master.setTarget_remarks(targetmaster.getTarget_remarks());
-		return targetservice.save(master);
+	public TargetMaster UpdateTarget(@PathVariable String targetode, @RequestBody TargetMaster targetmaster) {
+
+		if (targetservice.existsById(targetode)) {
+
+			TargetMaster master = targetservice.findById(targetode).orElseThrow();
+			master.setStart_date(targetmaster.getStart_date());
+			master.setActive_flag(targetmaster.getActive_flag());
+			master.setEnd_date(targetmaster.getEnd_date());
+			master.setTarget_description(targetmaster.getTarget_description());
+			master.setParent_code(targetmaster.getParent_code());
+			master.setTarget_remarks(targetmaster.getTarget_remarks());
+			return targetservice.save(master);
+		} else {
+			return targetservice.save(targetmaster);
+		}
+
 	}
+	@DeleteMapping("api/Delete/{targetode}")
+	public Map<String ,Boolean>DeleteByTarget(@PathVariable String targetode){
+		System.out.println(targetode);
+		TargetMaster master = targetservice.findById(targetode).orElseThrow();
+		targetservice.delete(master);
+		Map<String,Boolean> ResponseEntity =new HashMap<>();
+		ResponseEntity.put("deleted", Boolean.TRUE);
+		return ResponseEntity;
+	}
+		
+	
 }
